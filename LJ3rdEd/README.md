62e74b17a68af2d026276c31425f30efbb682a9a
reduviidae <wiljago@gmail.com>
2017-04-22 16:47:32 -0400

    first linting passed

--- LJ3rdEd/README.md
+++
@@ -1,2 +0,0 @@
-# LJ3rdEd
-Nothing to see here. It's just me working through Learning Javascript 3rd Edition
---
+++ public/dist/test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var sentences = [{
+    subject: 'Bananas',
+    verb: 'are',
+    object: 'non-dogs'
+}, {
+    subject: 'Snek',
+    verb: 'are',
+    object: 'long'
+}];
+
+function say(_ref) {
+    var subject = _ref.subject,
+        verb = _ref.verb,
+        object = _ref.object;
+
+    // es6 feature: template strings
+    console.log(subject + ' ' + verb + ' ' + object);
+}
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = sentences[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var s = _step.value;
+
+        say(s);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
